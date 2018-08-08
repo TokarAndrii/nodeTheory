5a59e6e6c7cfc28bd3cd5750b43f423765d14d89
tokarandrii <tokar.andrii@gmail.com>
2018-08-08 17:51:31 +0300

    CRUD operations with users

--- module4/index.js
+++ module4/index.js
@@ -1,3 +1,5 @@
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 const app = require('express')();
 /*
 same as:
@@ -10,28 +12,52 @@ const config = require('./config/development.json');
 app.listen(config.port);
 console.log(`listen port ${config.port}`);
 
-//Controller
-//endpoint url + method
-const getUsers = (req, res, next) => {
-    req.users = [
-        {
-            username: "user1",
-            password: "pass1"
-        },
-        {
-            username: "user2",
-            password: "pass2"
-        },
-        {
-            username: "user3",
-            password: "pass3"
-        },
-        {
-            username: "user4",
-            password: "pass4"
-        },
-    ];
+const USERS = require('./mock-data/users');
 
+
+app.use((req, res, next) => {
+    console.log(`${req.url} --> ${req.method}, Date:${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}`);
+    next();
+});
+
+app.use(bodyParser.json()); //for parsing application/json
+app.use(bodyParser.urlencoded({extend: true})); //for parsing application/
+
+//Controllers
+//endpoint url + method
+//getUsers - middleware
+const getUsers = (req, res, next) => {
+    req.users = USERS;
+    next();
+};
+
+const getOneUser = (req, res, next) => {
+    const {index} = req.params;
+
+    req.users = USERS[index];
+
+    req.index = index;//set index for get it at next middleware
+
+    next();
+};
+
+const updateUser = (req, res, next) => {
+    const user = req.body;
+
+    const newUser = _.merge(req.users, user);
+
+    //get index from getOneUser
+    USERS[req.index] = newUser;
+
+    req.users = USERS;
+
+    next();
+
+};
+
+const deleteUser = (req, res, next) => {
+    USERS.splice(req.index, 1);
+    req.users = USERS;
     next();
 };
 
@@ -42,19 +68,47 @@ const sendUsers = (req, res, next) => {
     res.json(req.users);
 };
 
-app.use((req, res, next) => {
-    console.log(`${req.url} --> ${req.method}, Date:${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}`);
+
+const addUser = (req, res, next) => {
+    const user = req.body;
+    USERS.push(user);
+    req.users = USERS;
     next();
-});
+};
 
+const getBooks = (req, res, next) => {
+    const {index} = req.params;
+    console.log(`user index - ${index}`);
+    req.books = USERS[index].books;
+    next();
+};
+const sendBooks = (req, res, next) => {
+    res.status(200);
+    res.json(req.books);
+    next();
+};
 
+//Users
 app.get('/users/', getUsers, sendUsers);
-//getUsers - middleware
 
-app.post('/users/');
-app.put('/users/');
-app.delete('/users/');
+app.post('/users/', addUser, sendUsers);
+
+app.put('/users/:index', getOneUser, updateUser, sendUsers); //use Lodash _.merge
+
+app.delete('/users/:index', getOneUser, deleteUser, sendUsers);
 //app.all('/users/')
+
+
+//Books
+app.get('/users/:index/books', getBooks, sendBooks);
+
+app.post('/users/:index/books');
+
+app.put('/users/:index/books/:title',);
+
+app.delete('/users/:index/books/:title');
+
+app.get('/users/:index/books/:title');
 
 
 //Not found error
